c9164098effb4b6aa6107b065d27a98202cf1f64
mandoon <563822080@qq.com>
2021-03-05 22:07:34 +1300

    resolver forgotpassword

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -34,6 +34,51 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Mutation(() => UserResponse)
+  async changePassword(
+    @Arg('token') token: string,
+    @Arg('newPassword') newPassword: string,
+    @Ctx() { redis, em, req }: MyContext
+  ): Promise<UserResponse> {
+    if (newPassword.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "newPassword",
+            message: "should be longer than 2",
+          },
+        ]
+      };
+    }
+    const userId = await redis.get(FORGET_PASSWORD_PREFIX + token)
+    if (!userId) {
+      return {
+        errors: [
+          {
+            field: "token",
+            message: "token expired",
+          },
+        ]
+      }
+    }
+    const user: any = em.findOne(User, { id: parseInt(userId) })
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "token",
+            message: "user nolonger exists",
+          },
+        ]
+      }
+    }
+    user.password = await argon2.hash(newPassword);
+    await em.persistAndFlush(user)
+
+    req.session.userId = user.id;
+    return { user }
+  }
+
   @Mutation(() => Boolean)
   async forgotPassword(
     @Arg("email") email: string,
